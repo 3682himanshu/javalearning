92242c9a399db37dbc0c27d3419309be5374c9c7
Hi-Guleria <guleria3682@gmail.com>
2022-07-25 16:09:58 +0530

    added dependencies and test

---
+++ src/test/java/com/mercury/airports/test/AirportsApplicationTest.java
@@ -0,0 +1,14 @@
+package com.mercury.airports.test;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class AirportsApplicationTest {
+
+    @Test
+    void testFindAirportByThrowsExceptionWhenInputInvalid(){
+        fail("Not yet Implemented");
+    }
+}
